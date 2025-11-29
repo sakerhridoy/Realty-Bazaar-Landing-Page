ce2b6a87f6d409a55731c9b9599ee3fb14b718c0
hridoy <sakerahmedhridoy@gmail.com>
2025-11-29 20:35:02 +0600

    fix slider bug

--- src/Components/Invest/Invest.jsx
+++ src/Components/Invest/Invest.jsx
@@ -13,6 +13,48 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 const Invest = () => {
+  const slides = [
+    {
+      id: 'slide-01',
+      title: 'Project detail title 01',
+      rating: '4.5',
+      reviews: '(100)',
+      developer: 'By Test 01 Developers',
+      image1: invest1,
+      image2: invest12,
+      description1:
+        'Lorem ipsum dolor sit amet consectetur. Suspendisse quis arcu suspendisse eget interdum vestibulum urna. Vel risus nisi quis sed neque.',
+      description2:
+        'Ridiculus facilisis ipsum sit eu neque ultricies fusce dolor pellentesque. Eget pellentesque suscipit mattis rutrum.',
+    },
+    {
+      id: 'slide-02',
+      title: 'Project detail title 02',
+      rating: '4.3',
+      reviews: '(100)',
+      developer: 'By Test 02 Developers',
+      image1: invest2,
+      image2: invest22,
+      description1:
+        'Lorem ipsum dolor sit amet consectetur. Suspendisse quis arcu suspendisse eget interdum vestibulum urna. Vel risus nisi quis sed neque.',
+      description2:
+        'Ridiculus facilisis ipsum sit eu neque ultricies fusce dolor pellentesque. Eget pellentesque suscipit mattis rutrum.',
+    },
+    {
+      id: 'slide-03',
+      title: 'Project detail title 03',
+      rating: '4.5',
+      reviews: '(100)',
+      developer: 'By Test 01 Developers',
+      image1: invest1,
+      image2: invest12,
+      description1:
+        'Lorem ipsum dolor sit amet consectetur. Suspendisse quis arcu suspendisse eget interdum vestibulum urna. Vel risus nisi quis sed neque.',
+      description2:
+        'Ridiculus facilisis ipsum sit eu neque ultricies fusce dolor pellentesque. Eget pellentesque suscipit mattis rutrum.',
+    },
+  ];
+
   return (
     <>
       <section className="py-[95px]">
@@ -21,7 +63,7 @@ const Invest = () => {
             <img src={bg} alt="Background" className="w-full" />
           </div>
           <div className="container">
-            <div className="flex gap-[107px] py-[90px] px-[70px]">
+            <div className="flex justify-between  py-[90px] px-[70px]">
               {' '}
               <div className="left pb-12">
                 <div className="headline pb-[22px]">
@@ -38,140 +80,51 @@ const Invest = () => {
                   spaceBetween={30}
                   slidesPerView={2}
                   loop={true}
-                  className="w-[858px] pb-12"
+                  className="w-[790px] pb-12"
                 >
-                  {/* Slide 1 */}
-                  <SwiperSlide>
-                    <div className="item w-[414px]">
-                      <h4 className="font-arial font-bold text-xl text-[#072135]">
-                        Project detail title 01
-                      </h4>
-                      <div className="flex items-center gap-1 pb-[17px]">
-                        <TiStarFullOutline className="text-[#FFD900] text-lg" />
-                        <h6 className="font-arial font-semibold text-sm text-[#072135] pt-0.5">
-                          4.5
-                        </h6>
-                        <p className="font-arial font-medium text-sm text-[#666666] pt-0.5">
-                          (100)
-                        </p>
-                        <ul className="ms-6">
-                          <li className="list-disc">
-                            <p className="font-semibold text-sm text-[#333333] pt-0.5">
-                              By Test 01 Developers
-                            </p>
-                          </li>
-                        </ul>
-                      </div>
-                      <div className="innerItem">
-                        <div className="flex gap-[30px] pb-[19px]">
-                          <img src={invest1} alt="invest" className="w-48" />
-                          <img src={invest12} alt="invest" className="w-48" />
+                  {slides.map(s => (
+                    <SwiperSlide key={s.id}>
+                      <div className="item w-[370px]">
+                        <h4 className="font-arial font-bold text-xl text-[#072135]">
+                          {s.title}
+                        </h4>
+                        <div className="flex items-center gap-1 pb-[17px]">
+                          <TiStarFullOutline className="text-[#FFD900] text-lg" />
+                          <h6 className="font-arial font-semibold text-sm text-[#072135] pt-0.5">
+                            {s.rating}
+                          </h6>
+                          <p className="font-arial font-medium text-sm text-[#666666] pt-0.5">
+                            {s.reviews}
+                          </p>
+                          <ul className="ms-6">
+                            <li className="list-disc">
+                              <p className="font-semibold text-sm text-[#333333] pt-0.5">
+                                {s.developer}
+                              </p>
+                            </li>
+                          </ul>
                         </div>
-                        <article>
-                          <p className="font-arial text-base text-[#333333] leading-[22px]">
-                            Lorem ipsum dolor sit amet consectetur. Suspendisse
-                            quis arcu suspendisse eget interdum vestibulum urna.
-                            Vel risus nisi quis sed neque.
-                          </p>
-                          <p className="font-arial text-base text-[#333333] leading-[22px] pt-8">
-                            Ridiculus facilisis ipsum sit eu neque ultricies
-                            fusce dolor pellentesque. Eget pellentesque suscipit
-                            mattis rutrum.
-                          </p>
-                        </article>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-
-                  {/* Slide 2 */}
-                  <SwiperSlide>
-                    <div className="item w-[414px]">
-                      <h4 className="font-arial font-bold text-xl text-[#072135]">
-                        Project detail title 02
-                      </h4>
-                      <div className="flex items-center gap-1 pb-[17px]">
-                        <TiStarFullOutline className="text-[#FFD900] text-lg" />
-                        <h6 className="font-arial font-semibold text-sm text-[#072135] pt-0.5">
-                          4.3
-                        </h6>
-                        <p className="font-arial font-medium text-sm text-[#666666] pt-0.5">
-                          (100)
-                        </p>
-                        <ul className="ms-6">
-                          <li className="list-disc">
-                            <p className="font-semibold text-sm text-[#333333] pt-0.5">
-                              By Test 02 Developers
+                        <div className="innerItem">
+                          <div className="flex gap-5 pb-[19px]">
+                            <img src={s.image1} alt="invest" className="w-42" />
+                            <img src={s.image2} alt="invest" className="w-42" />
+                          </div>
+                          <article>
+                            <p className="font-arial text-base text-[#333333] leading-[22px]">
+                              {s.description1}
                             </p>
-                          </li>
-                        </ul>
-                      </div>
-                      <div className="innerItem">
-                        <div className="flex gap-[30px] pb-[19px]">
-                          <img src={invest2} alt="invest" className="w-48" />
-                          <img src={invest22} alt="invest" className="w-48" />
-                        </div>
-                        <article>
-                          <p className="font-arial text-base text-[#333333] leading-[22px]">
-                            Lorem ipsum dolor sit amet consectetur. Suspendisse
-                            quis arcu suspendisse eget interdum vestibulum urna.
-                            Vel risus nisi quis sed neque.
-                          </p>
-                          <p className="font-arial text-base text-[#333333] leading-[22px] pt-8">
-                            Ridiculus facilisis ipsum sit eu neque ultricies
-                            fusce dolor pellentesque. Eget pellentesque suscipit
-                            mattis rutrum.
-                          </p>
-                        </article>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-
-                  {/* Slide 3 */}
-                  <SwiperSlide>
-                    <div className="item w-[414px]">
-                      <h4 className="font-arial font-bold text-xl text-[#072135]">
-                        Project detail title 03
-                      </h4>
-                      <div className="flex items-center gap-1 pb-[17px]">
-                        <TiStarFullOutline className="text-[#FFD900] text-lg" />
-                        <h6 className="font-arial font-semibold text-sm text-[#072135] pt-0.5">
-                          4.5
-                        </h6>
-                        <p className="font-arial font-medium text-sm text-[#666666] pt-0.5">
-                          (100)
-                        </p>
-                        <ul className="ms-6">
-                          <li className="list-disc">
-                            <p className="font-semibold text-sm text-[#333333] pt-0.5">
-                              By Test 01 Developers
+                            <p className="font-arial text-base text-[#333333] leading-[22px] pt-8">
+                              {s.description2}
                             </p>
-                          </li>
-                        </ul>
-                      </div>
-                      <div className="innerItem">
-                        <div className="flex gap-[30px] pb-[19px]">
-                          <img src={invest1} alt="invest" className="w-48" />
-                          <img src={invest12} alt="invest" className="w-48" />
+                          </article>
                         </div>
-                        <article>
-                          <p className="font-arial text-base text-[#333333] leading-[22px]">
-                            Lorem ipsum dolor sit amet consectetur. Suspendisse
-                            quis arcu suspendisse eget interdum vestibulum urna.
-                            Vel risus nisi quis sed neque.
-                          </p>
-                          <p className="font-arial text-base text-[#333333] leading-[22px] pt-8">
-                            Ridiculus facilisis ipsum sit eu neque ultricies
-                            fusce dolor pellentesque. Eget pellentesque suscipit
-                            mattis rutrum.
-                          </p>
-                        </article>
                       </div>
-                    </div>
-                  </SwiperSlide>
+                    </SwiperSlide>
+                  ))}
                 </Swiper>
                 {/* Swiper Slider End */}
               </div>
-              <div className="right">
+              <div className="right flex justify-end">
                 <div className="w-[335px] rounded-lg bg-[#072135] overflow-hidden h-full relative">
                   <div className="pt-8 ps-[27px] pe-[22px] z-20 relative">
                     <h3 className="text-white text-[22px] font-arial font-bold pb-3.5">
